8607d35a5d21391c83f38c988481d9058a6a604d
lpawlak1 <lukaszpawlak162@gmail.com>
2021-03-22 21:45:42 +0100

    laby 4

---
+++ c4/min.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <malloc.h>
+void insert(int *tab, int n){
+    // na idx 0 jest wartosc do przestawienia
+    for (int i = 0;i<n-1 && tab[i] < tab[i+1];i++){
+        int temp = tab[i];
+        tab[i] = tab[i+1];
+        tab[i+1] = temp;
+    }
+}
+int partition(int *tab,int left,int right){
+    int j = left;
+    for (int i = left;i<right;i++){
+        if (tab[i] > tab[right]){
+            int temp = tab[i];
+            tab[i] = tab[j];
+            tab[j] = temp;
+            j++;
+        }
+    }
+    int temp = tab[right];
+    tab[right] = tab[j];
+    tab[j] = temp;
+    return j;
+}
+void quicksort(int *tab,int left,int right){
+    if (left < right){
+        int q = partition(tab,left,right);
+        quicksort(tab,left,q-1);
+        quicksort(tab,q+1,right);
+    }
+}
+
+int main(){
+    int n,k;
+    scanf("%d%d",&n,&k);
+    int *tab = malloc(sizeof(int)*n);
+    for(int i =0;i<n;i++) scanf("%d",&tab[i]);
+
+    quickSort(tab,0,n-1);
+    /* for(int i =0;i<n;i++)printf("%d ",tab[i]); */
+    /* printf("\n"); */
+    for (int j = 0;j<k;j++){
+        tab[0] = tab[0]/2;
+        insert(tab,n);
+    }
+    int suma = 0;
+    for(int i =0;i<n;i++) suma += tab[i];
+    printf("%d",suma);
+    free(tab);
+}
---
+++ c4/pri.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <malloc.h>
+short isprime(int num){
+    if (num <= 1) return 0;
+    if(num == 2 || num == 3){
+        return 1;
+    }
+    if(num%2 ==0 || num%3==0) return 0;
+    int i = 5;
+    while (i*i<=num)
+    {
+        if(num%i==0 || num%(i+2)==0) return 0;
+        i+=6;
+    }
+    return 1;
+}
+short check(int num){
+    int cp = 0;
+    int nowa = num;
+    while(nowa != 0){
+        cp+=(nowa%10)*(nowa%10);
+        nowa/=10;
+    }
+    if (cp == 4){
+        return 0;
+    }
+    if (cp > 1){
+        return check(cp);
+    }
+    return 1;
+ 
+}
+int main(){
+    int l,u,k;
+    scanf("%d%d%d",&l,&u,&k);
+    int last = 0;
+    for(;l<=u && k>0;l++){
+        if(isprime(l) == 1){
+            if (check(l) == 1){
+                last = l;
+                k-=1;
+            }
+        }
+    }
+    if(k == 0){
+        printf("%d",last);
+    } 
+    else printf("-1");
+
+}
