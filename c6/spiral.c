798d93fcd7c305931d2229f3e7343b790d83294f
lpawlak1 <lukaszpawlak162@gmail.com>
2021-04-11 14:25:06 +0200

    prep for c6

---
+++ c6/spiral.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <malloc.h>
+
+void spiral(int i, int j, int l, int **a)
+{
+    while(i != 0){
+        int k = 0;
+
+        for (k = j; k < i; k++) a[j][k] = l++;
+        for (k = j; k < i; k++) a[k][i] = l++;
+        for (k = i; k > j; k--) a[i][k] = l++;
+        for (k = i; k > j; k--) a[k][j] = l++;
+
+        i--;
+        j++;
+    
+    }
+}
+
+int main(void)
+{
+    int n;
+    scanf("%d",&n);
+    int *a = (int *)malloc(n * n * sizeof(int));
+    int i, j;
+
+    /* for (i = 0; i < n; i++) */
+    /*     a[i] = (int *)malloc(n * sizeof(int)); */
+
+    i = n-1;
+    j = 0;
+    int l = 1;
+    while(i != 0){
+        int k = 0;
+        // poziom gora
+        for (k = j; k < i; k++) a[(j*n)+k] = l++;
+        // pion prawo
+        for (k = j; k < i; k++) a[(k*n)+i] = l++;
+        // poziom dol
+        for (k = i; k > j; k--) a[(i*n)+k] = l++;
+        // pion lewo
+        for (k = i; k > j; k--) a[(k*n)+j] = l++;
+        i--;
+        j++;
+    }
+    if (n % 2 == 1)
+        a[((n/2)*n)+(n/2)] = l;
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++)
+            printf("%d ", a[(i*n)+j]);
+        printf("\n");
+    }
+    return 0;
+}
